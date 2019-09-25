14d40904398e022c54e65bf36b1a2982ff95df10
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-25 20:48:02 +0000

    Function that pushes an element to the stack

--- op_push.c
+++ op_push.c
@@ -1,42 +1,48 @@
 #include "monty.h"
+/**
+* op_push - function that pushes an element to the stack
+* @stack: double linked list that allocate data
+* @line_number: line of file that have been read
+*/
 void op_push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *newnode = NULL;
-    int n;
-    char *auxstrtok;
-    unsigned j = 0;
+	stack_t *newnode = NULL;
+	int n;
+	char *auxstrtok;
+	unsigned int j = 0;
 
-    newnode = malloc(sizeof(stack_t));
-    if (!newnode)
-    {
-        fprintf(stderr, "Error: malloc failed");
-        exit (EXIT_FAILURE);
-    }
-    if ((auxstrtok = strtok(NULL, DELIMITER)) == NULL)
-    {
-        fprintf(stderr, "L%d: usage: push integer", line_number);
-        exit (EXIT_FAILURE);
-    }
-    for (j = 0; j < strlen(auxstrtok); j++)
-        if (isdigit(auxstrtok[j]) == 0)
-        {
-            fprintf(stderr, "L%d: usage: push integer", line_number);
-            exit (EXIT_FAILURE);
-        }
+	newnode = malloc(sizeof(stack_t));
+	if (!newnode)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	auxstrtok = strtok(NULL, DELIMITER);
+	if (auxstrtok  == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
+	for (j = 0; j < strlen(auxstrtok); j++)
+		if (isdigit(auxstrtok[j]) == 0)
+		{
+			fprintf(stderr, "L%d: usage: push integer", line_number);
+			exit(EXIT_FAILURE);
+		}
 
-    n = atoi(auxstrtok);
-    newnode->next = NULL;
-    newnode->prev = NULL;
-    newnode-> n= n;
-    if (*stack == NULL)
-    {
-        *stack = newnode;
-        return;
-    }
-    newnode->next = *stack;
-    (*stack)->prev = newnode;
-    *stack = newnode;
-    return;
+	n = atoi(auxstrtok);
+	newnode->next = NULL;
+	newnode->prev = NULL;
+	newnode->n = n;
+	if (*stack == NULL)
+	{
+		*stack = newnode;
+		return;
+	}
+	newnode->next = *stack;
+	(*stack)->prev = newnode;
+	*stack = newnode;
+	return;
 
 
 }
