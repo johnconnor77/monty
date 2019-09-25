d5d5b3e30dc64ac4cfdcea08038644fc69b47682
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-25 20:40:48 +0000

    function that pushes an element to the stack

--- op_pall.c
+++ op_pall.c
@@ -1,15 +1,21 @@
 #include "monty.h"
+/**
+* op_pall - function that prints all the values
+* on the stack starting from the top of the stack.
+* @stack: double linkes list that allocate data
+* @line_number: line of file that have been read
+*
+*/
 void op_pall(stack_t **stack, unsigned int line_number)
 {
-    UNUSED(line_number);
-    stack_t *copystack = NULL;
+	UNUSED(line_number);
+	stack_t *copystack = NULL;
 
-    copystack = *stack;
-    while(copystack)
-    {
-            printf("%d\n",copystack->n);
-            copystack = copystack->next;
-    }
-    return;
+	copystack = *stack;
+	while (copystack)
+	{
+		printf("%d\n", copystack->n);
+		copystack = copystack->next;
+	}
 }
 
