d57d4acb9ed0c6e1b82b551da5dd144f97391304
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-25 21:01:47 +0000

    Function that allows to search the correct function pointer

--- get_op_functions.c
+++ get_op_functions.c
@@ -6,25 +6,25 @@
 */
 instruction_t *get_op(const char *input)
 {
-    int i = 0;
-    instruction_t *opt = NULL;
-    instruction_t ops[] = {
-            {"push", op_push},
-            {"pall", op_pall},
-            {NULL, NULL}
-    };
+	int i = 0;
+	instruction_t *opt = NULL;
+	instruction_t ops[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{NULL, NULL}
+	};
 
-    while (ops[i].opcode && strcmp(ops[i].opcode, input) != 0)
-        i++;
+	while (ops[i].opcode && strcmp(ops[i].opcode, input) != 0)
+		i++;
 
-    opt = malloc(sizeof(instruction_t));
-    if (opt == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit (EXIT_FAILURE);
-    }
+	opt = malloc(sizeof(instruction_t));
+	if (opt == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    opt->opcode = ops[i].opcode;
-    opt->f = ops[i].f;
-    return (opt);
+	opt->opcode = ops[i].opcode;
+	opt->f = ops[i].f;
+	return (opt);
 }
