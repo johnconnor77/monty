1e6714ea6d6353bafdc15b293018c534620e08c4
jalondono <juanlondono151776@hotmail.com>
2019-09-26 15:35:02 -0500

    function that prints the string starting at the top of the stack, followed by a new

--- op_pstr.c
+++ op_pstr.c
@@ -1,4 +1,38 @@
-//
-// Created by jalondono on 26/09/19.
-//
+#include "monty.h"
+/**
+* op_pstr - f prints the char at the top of the stack
+* @stack: double linkes list that allocate data
+* @line_number: line of file that have been read
+*/
+void op_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *copystack = *stack;
+	int n;
+
+	UNUSED(line_number);
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "\n");
+	}
+
+	while (copystack && copystack->n != '\0')
+	{
+		n = copystack->n;
+
+		if (!isascii(n))
+		{
+			break;
+		}
+		printf("%c", copystack->n);
+
+		copystack = copystack->next;
+	}
+
+	fprintf(stderr, "\n");
+
+
+
+}
+
 
