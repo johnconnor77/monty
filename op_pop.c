8151987326c2654f79a1163faa6bf401d25a9a7b
jalondono <juanlondono151776@hotmail.com>
2019-09-26 10:08:42 -0500

    added op_pop function

--- op_pop.c
+++ op_pop.c
@@ -10,7 +10,7 @@ void op_pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
