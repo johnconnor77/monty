cd7eefd43c439fc4c1d8a2182ae16836875c5c2f
jalondono <juanlondono151776@hotmail.com>
2019-09-26 10:52:44 -0500

    function that swaps the top two elements of the stack.

--- op_swap.c
+++ op_swap.c
@@ -1,12 +1,23 @@
 #include "monty.h"
 /**
-* op_swap - function that delete top value
+* op_swap - function that changes the top value
+ * with the next one, just values not address
 * @stack: double linkes list that allocate data
 * @line_number: line of file that have been read
 */
 void op_swap(stack_t **stack, unsigned int line_number)
 {
-	UNUSED(stack);
-	UNUSED(line_number);
+	int copynumstack = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	copynumstack = (*stack)->next->n;
+	(*stack)->next->n = (*stack)->n;
+	(*stack)->n = copynumstack;
+
 }
 
