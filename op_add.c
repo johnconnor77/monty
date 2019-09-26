210408ddb04d0677b55e2d1eb8df98605f9cb43c
jalondono <juanlondono151776@hotmail.com>
2019-09-26 11:09:48 -0500

    Corrected print message error

--- op_add.c
+++ op_add.c
@@ -12,7 +12,7 @@ void op_add(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
