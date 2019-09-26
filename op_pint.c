063ee17c0ea0d95c29220e7354fecac01ddeb417
jalondono <juanlondono151776@hotmail.com>
2019-09-26 09:35:19 -0500

    added op_pint functions

--- op_pint.c
+++ op_pint.c
@@ -10,7 +10,7 @@ void op_pint(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", copystack->n);
