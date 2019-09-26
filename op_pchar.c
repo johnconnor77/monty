e78b43e94d211c59c20b7c22c622dbe19899eb6f
jalondono <juanlondono151776@hotmail.com>
2019-09-26 13:20:16 -0500

    upload op_pchar fuction

--- op_pchar.c
+++ op_pchar.c
@@ -10,12 +10,12 @@ void op_pchar(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d:: can't pchar, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+	if ((*stack)->n > 127)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", copystack->n);
