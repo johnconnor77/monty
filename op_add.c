1196488c29ca024e0ad24ea92fa45edc3d91046b
jalondono <juanlondono151776@hotmail.com>
2019-09-26 11:06:57 -0500

    Function that adds the top two elements of the stack

--- op_add.c
+++ op_add.c
@@ -1,11 +1,25 @@
 #include "monty.h"
 /**
-* op_add - function that delete top value
+* op_add - function that sums top value
+ * with the next one
 * @stack: double linkes list that allocate data
 * @line_number: line of file that have been read
 */
 void op_add(stack_t **stack, unsigned int line_number)
 {
-	UNUSED(stack);
-	UNUSED(line_number);
+	int copynumstack1, copynumstack2, sum;
+
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	copynumstack1 = (*stack)->n;
+	copynumstack2 = (*stack)->next->n;
+	sum = copynumstack1 + copynumstack2;
+
+	op_pop(stack, line_number);
+	(*stack)->n = sum;
 }
